437368cd0403b317ec8c027babb5749950ff0b72
Bob van Schendel <bobyantius@hotmail.com>
2015-12-10 12:17:55 +0100

    cleaned up the update methods

---
+++ SmallPracticeGame/Logic.fs
@@ -0,0 +1,33 @@
+﻿module Logic
+
+open StateMonad
+open Utility
+open Actors
+
+let Positions : State<(Vector2<int>*Vector2<int>) list, GameState<Box>> =
+  fun gs ->
+    let gs' = {gs with Boxes = [for x in gs.Boxes -> x.Move]}
+    let LocandSize =  [for x in gs'.Boxes ->
+                          {X = int <| x.Pos.X;Y = int <| x.Pos.Y},
+                          {X = int <| x.Size.X;Y = int <| x.Size.Y}
+                      ]
+    (LocandSize,gs')
+
+let makeSprites dimensions :State<Map<(int*int), string> , GameState<Box>> =
+  fun gs ->
+    let looper loc size =
+      [
+        for i = loc.X to loc.X + size.X do
+          for p = loc.Y to loc.Y + size.Y do
+            yield ((i, p), "X")
+      ]
+    let rec createList lslist xs =
+      match lslist with
+      | [] -> xs
+      | h::t -> let loc, size = fst(h), snd(h)
+                let ys = looper loc size
+                let xs' = ys @ xs
+                createList t xs'
+    let LocandSize' = createList dimensions []
+    let sprites = Map.ofList LocandSize'
+    (sprites, gs)
--- SmallPracticeGame/Program.fs
+++ SmallPracticeGame/Program.fs
@@ -3,39 +3,18 @@ open System
 open StateMonad
 open Utility
 open Actors
+open Logic
 
 type Sprites = Map<(int*int), string>
 
-let UpdateGS : State<Sprites, GameState<Box>> =
-  fun gs ->
-    let gs' = {gs with Boxes = [for x in gs.Boxes -> x.Move]}
-    let LocandSize = [for x in gs'.Boxes ->
-                          {X = int <| x.Pos.X;Y = int <| x.Pos.Y},
-                          {X = int <| x.Size.X;Y = int <| x.Size.Y}
-                    ]
-    let looper loc size =
-      [
-        for i = loc.X to loc.X + size.X do
-          for p = loc.Y to loc.Y + size.Y do
-            yield ((i, p), "X")
-      ]
-    let rec createList lslist xs =
-      match lslist with
-      | [] -> xs
-      | h::t -> let loc, size = fst(h), snd(h)
-                let ys = looper loc size
-                let xs' = ys @ xs
-                createList t xs'
-    let LocandSize' = createList LocandSize []
-    let sprites = Map.ofList LocandSize'
-    (sprites, gs')
-
-let MainUpdate() : State<Unit, GameState<Box>> =
+let UpdateGS : State<Unit, GameState<Box>> =
   state{
-    let! sprites = UpdateGS
+    let! locations = Positions
+    let! sprites = makeSprites locations
     do! Draw sprites
     return ()
   }
+
 let beginstate =  { Boxes =
                     [(createBox {X = 1.0;Y = 1.0});
                   (*(createBox {X = 50.0;Y = 20.0});
@@ -45,7 +24,7 @@ let beginstate =  { Boxes =
                     ]
                   }
 let rec GameLoop prevGameState =
-  let tentativeGameState = snd(MainUpdate() prevGameState)
+  let tentativeGameState = snd(UpdateGS prevGameState)
   Thread.Sleep(100)
   GameLoop tentativeGameState
 
